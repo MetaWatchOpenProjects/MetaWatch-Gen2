MetaWatchOpenProjects/MetaWatch-Gen2

commit 70ea9139285e73e2431b1c58644d1e34d803fe8a

Merge pull request: Show year in 24h mode on digital watch.

Watch/Application/LcdDisplay.c
@@ -1782,11 +1782,11 @@ static void DisplayAmPm(void)
 
 static void DisplayDayOfWeek(void)
 {
-  int DayOfWeek = RTCDOW;
+  //int DayOfWeek = RTCDOW;
   
-  unsigned char const *pFoo = DaysOfWeek[DayOfWeek];
+  //unsigned char const *pFoo = DaysOfWeek[DayOfWeek];
     
-  WriteFoo(pFoo,10,8);
+  WriteFoo(DaysOfWeek[RTCDOW], GetTimeFormat() == TWENTY_FOUR_HOUR ? 0 : 10, 8);
   
 }
 
@@ -1814,12 +1814,27 @@ static void DisplayDate(void)
     gColumn = 8;
     gBitColumnMask = BIT1;
     SetFont(MetaWatch7);
+
+    if ( GetTimeFormat() == TWENTY_FOUR_HOUR )
+    {
+      int year = RTCYEAR;
+      WriteFontCharacter(year/1000+'0');
+      year %= 1000;
+      WriteFontCharacter(year/100+'0');
+      year %= 100;
+      WriteFontCharacter(year/10+'0');
+      year %= 10;
+      WriteFontCharacter(year+'0');
+      gRow = 12;
+    }
+
+    gColumn = 8;
+    gBitColumnMask = BIT1;
     WriteFontCharacter(First/10+'0');
     WriteFontCharacter(First%10+'0');
     WriteFontCharacter('/');
     WriteFontCharacter(Second/10+'0');
     WriteFontCharacter(Second%10+'0');
-    
   }
 }
 

commit 33df914709eeccdf6fa888f43fc18fd474674b67

Fix erroneous display in 24 hour mode.
The "Show year in 24h mode" pull request (#14) was broken in commit 70ea9139285e73e2431b1c58644d1e34d803fe8a, causing the day of week and date to be printed on the same row.

This commit fixes the bug using code from commit 70ea9139285e73e2431b1c58644d1e34d803fe8a.

Watch/Application/LcdDisplay.c
@@ -1782,8 +1782,8 @@ static void DisplayAmPm(void)
 
 static void DisplayDayOfWeek(void)
 {
-  /* row offset = 10 , column offset = 8 */
-  WriteIcon4w10h(DaysOfWeek[RTCDOW],10,8);
+  /* row offset = 0 or 10 , column offset = 8 */
+  WriteIcon4w10h(DaysOfWeek[RTCDOW], GetTimeFormat() == TWENTY_FOUR_HOUR ? 0 : 10, 8);
 }
 
 static void DisplayDate(void)